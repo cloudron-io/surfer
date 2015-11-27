faffabc036f362beab87d507246d7e0698eb39b4
Girish Ramakrishnan <girish@forwardbias.in>
2015-11-27 14:26:07 -0800

    fix typo

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ export NODE_ENV=production
 
 chown -R cloudron:cloudron /app/data
 
-/usr/local/bin/gosu cloudron:cloudron || node /app/code/app.js /app/data
+/usr/local/bin/gosu cloudron:cloudron node /app/code/app.js /app/data
