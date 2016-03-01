d3312ed1aace3c72570f60be56d846fb9ecbc584
Johannes Zellner <johannes@nebulon.de>
2016-03-01 15:10:25 +0100

    Make directory listing navigatable

--- app/js/app.js
+++ app/js/app.js
@@ -20,6 +20,8 @@ function login(username, password) {
         // clearly not the best option
         localStorage.username = username;
         localStorage.password = password;
+
+        loadDirectory(app.path);
     });
 }
 
@@ -32,20 +34,61 @@ function logout() {
     delete localStorage.password;
 }
 
+function sanitize(filePath) {
+    filePath = '/' + filePath;
+    return filePath.replace(/\/+/g, '/');
+}
+
+function loadDirectory(filePath) {
+    app.busy = true;
+
+    filePath = filePath ? sanitize(filePath) : '/';
+
+    console.log(filePath);
+
+    superagent.get('/api/files/' + filePath).query({ username: app.session.username, password: app.session.password }).end(function (error, result) {
+        app.busy = false;
+
+        if (error) return console.error(error);
+        if (result.statusCode === 401) return logout();
+
+        app.entries = result.body.entries;
+        app.path = filePath;
+        app.pathParts = filePath.split('/').filter(function (e) { return !!e; });
+        console.log(app.pathParts)
+    });
+}
+
+function open(entry) {
+    var path = sanitize(app.path + '/' + entry.filePath);
+
+    if (entry.isDirectory) return loadDirectory(path);
+
+    window.location.href = window.location.origin + path;
+}
+
+function up() {
+    loadDirectory(app.path.split('/').slice(0, -1).filter(function (p) { return !!p; }).join('/'));
+}
+
 var app = new Vue({
     el: '#app',
     data: {
         busy: true,
+        path: '/',
+        pathParts: [],
         session: {
             valid: false
         },
-        loginData: {
-
-        }
+        loginData: {},
+        entries: []
     },
     methods: {
         login: login,
-        logout: logout
+        logout: logout,
+        loadDirectory: loadDirectory,
+        open: open,
+        up: up
     }
 });
 
