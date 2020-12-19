a486591637a8368f1c7a0d35746ea6ea9dc96447
Johannes Zellner <johannes@cloudron.io>
2020-12-19 21:04:31 +0100

    Only keep comments in one block

--- vue.config.js
+++ vue.config.js
@@ -16,17 +16,10 @@ module.exports = {
       chunks: ['chunk-vendors', 'chunk-common', 'index']
     },
     public: {
-      // entry for the page
       entry: 'frontend/public.js',
-      // the source template
       template: 'frontend/public.html',
-      // output as dist/public.html
       filename: 'public.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
       title: 'Public Page',
-      // chunks to include on this page, by default includes
-      // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'public']
     }
   }
