fcf9b5c2fec1c26f90d9a3de12e4b1b41a7b06ac
Johannes Zellner <johannes@cloudron.io>
2021-03-17 19:20:57 +0100

    Ensure uncommon text types are sent as text/plain

--- src/mime.js
+++ src/mime.js
@@ -4,6 +4,19 @@ var fs = require('fs');
 
 var GLOBS2_FILE = '/usr/share/mime/globs2';
 
+// to override OTHER text types to text/plain
+var COMMON_TEXT_TYPES = [
+    'text/calendar',
+    'text/comma',
+    'text/css',
+    'text/html',
+    'text/javascript',
+    'text/plain',
+    'text/richtext',
+    'text/rtf',
+    'text/xml'
+];
+
 exports = module.exports = function (express) {
     console.log(`Loading rich mime-types from ${GLOBS2_FILE}`);
 
@@ -24,8 +37,12 @@ exports = module.exports = function (express) {
         var f = line.split(':');
         if (f.length <= 1) return;
 
-        if (!types[f[1]]) types[f[1]] = [];
-        types[f[1]].push(f[2].slice(2));
+        var type = f[1];
+
+        if (type.startsWith('text/') && COMMON_TEXT_TYPES.indexOf(type) === -1) type = 'text/plain';
+
+        if (!types[type]) types[type] = [];
+        types[type].push(f[2].slice(2));
     });
 
     Object.keys(types).forEach(function (type) {
@@ -34,4 +51,5 @@ exports = module.exports = function (express) {
 
         express.static.mime.define(obj);
     });
+
 };
