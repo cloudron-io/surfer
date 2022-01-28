78f17beee9ee6d13d48192d296684364c62fc405
Johannes Zellner <johannes@cloudron.io>
2022-01-28 14:27:52 +0100

    Update further modules

--- test/test.js
+++ test/test.js
@@ -71,6 +71,7 @@ describe('Application life cycle test', function () {
     // tests which are used more than once
     function login(done) {
         browser.manage().deleteAllCookies();
+        console.log(proto + app.fqdn + '/_admin')
         browser.get(proto + app.fqdn + '/_admin');
 
         waitForElement(By.id('usernameInput')).then(function () {
@@ -218,7 +219,7 @@ describe('Application life cycle test', function () {
     }
 
     xit('build app', function () { execSync('cloudron build', EXEC_ARGS); });
-    it('install app', function () { execSync(`cloudron install --location ${LOCATION}`, EXEC_ARGS); });
+    // it('install app', function () { execSync(`cloudron install --location ${LOCATION}`, EXEC_ARGS); });
 
     it('can get app information', getAppInfo);
 
