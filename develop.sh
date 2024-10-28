654c80e5754c5b401def24cc32e9552a12a436cb
Johannes Zellner <johannes@cloudron.io>
2024-10-28 19:13:25 +0100

    Make it work against a test cloudron

--- develop.sh
+++ develop.sh
@@ -2,5 +2,11 @@
 
 set -eu
 
+# Create a new oidc client in your test cloudron with and fill in the blanks
+export CLOUDRON_OIDC_ISSUER="https://my.nebulon.space"
+export CLOUDRON_APP_ORIGIN="http://localhost:3000"
+export CLOUDRON_OIDC_CLIENT_ID="surfer"
+export CLOUDRON_OIDC_CLIENT_SECRET="secret"
+
 echo "=> Start surfer"
 node ./server.js
