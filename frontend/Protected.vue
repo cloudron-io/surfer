8e713a5d7826d277ddea674c3045d8d5667b0333
Johannes Zellner <johannes@cloudron.io>
2024-10-29 13:35:37 +0100

    Move protected login to pankow

--- frontend/Protected.vue
+++ frontend/Protected.vue
@@ -2,77 +2,80 @@
   <div class="login-container" v-show="ready">
     <h1>Login to {{ settings.title }}</h1>
     <form @submit="onLogin" @submit.prevent v-show="settings.accessRestriction === 'password'">
-      <div class="p-fluid">
-        <div>
-          <label for="passwordInput">Password</label>
-          <Password inputId="passwordInput" :feedback="false" v-model="password" :class="{ 'p-invalid': error }"/>
-          <small v-show="error" :class="{ 'p-invalid': error }">Wrong username or password.</small>
-        </div>
+      <div>
+        <label for="passwordInput">Password</label>
+        <PasswordInput id="passwordInput" :feedback="false" v-model="password" :class="{ 'has-error': error }"/>
+        <small v-show="error" :class="{ 'has-error': error }">Wrong username or password.</small>
       </div>
-      <Button type="submit" label="Login" id="loginButton"/>
+      <Button @click="onLogin" id="loginButton" :loading="busy" :disabled="busy || !password">Login</Button>
     </form>
-    <a :href="'/api/oidc/login?returnTo=' + returnTo" v-show="settings.accessRestriction !== 'password'"><Button class="p-button-sm" label="Login with Cloudron" icon="pi pi-sign-in"/></a>
+    <Button :href="'/api/oidc/login?returnTo=' + returnTo" v-show="settings.accessRestriction !== 'password'" icon="pi pi-sign-in">Login with Cloudron</Button>
   </div>
 </template>
 
 <script>
 
+import { Button, PasswordInput } from 'pankow';
 import superagent from 'superagent';
 
 const ORIGIN = window.location.origin;
 
 export default {
-    name: 'ProtectedView',
-    data() {
-        return {
-            ready: false,
-            busy: true,
-            origin: ORIGIN,
-            error: false,
-            returnTo: '/',
-            password: '',
-            settings: {
-                accessRestriction: '',
-                title: ''
-            }
-        };
-    },
-    methods: {
-        onLogin: function () {
-            this.busy = true;
-            this.error = false;
+  name: 'ProtectedView',
+  components: {
+    Button,
+    PasswordInput
+  },
+  data() {
+    return {
+      ready: false,
+      busy: false,
+      origin: ORIGIN,
+      error: false,
+      returnTo: '/',
+      password: '',
+      settings: {
+        accessRestriction: '',
+        title: ''
+      }
+    };
+  },
+  mounted() {
+    // ensure we end up in the target destination after oidc login
+    this.returnTo = window.location.pathname || '/';
 
-            superagent.post(`${this.origin}/api/protectedLogin`).send({ password: this.password }).end((error, result) => {
-                this.busy = false;
+    superagent.get(`${this.origin}/api/settings`).end((error, result) => {
+      if (error) console.error(error);
 
-                if (error || result.statusCode !== 200) {
-                    console.error(error);
-                    this.password = '';
-                    this.error = true;
-                    return;
-                }
+      this.settings.title =  result.body.title;
+      this.settings.accessRestriction =  result.body.accessRestriction;
 
-                window.location.reload();
-            });
+      window.document.title = this.settings.title;
+
+      this.ready = true;
+
+      this.$nextTick(() => document.getElementById('passwordInput').focus());
+    });
+  },
+  methods: {
+    onLogin() {
+      this.busy = true;
+      this.error = false;
+
+      superagent.post(`${this.origin}/api/protectedLogin`).send({ password: this.password }).end((error, result) => {
+        this.busy = false;
+
+        if (error || result.statusCode !== 200) {
+          console.error(error);
+          this.password = '';
+          this.error = true;
+          return;
         }
-    },
-    mounted() {
-        // ensure we end up in the target destination after oidc login
-        this.returnTo = window.location.pathname || '/';
 
-        superagent.get(`${this.origin}/api/settings`).end((error, result) => {
-            if (error) console.error(error);
-
-            this.settings.title =  result.body.title;
-            this.settings.accessRestriction =  result.body.accessRestriction;
-
-            window.document.title = this.settings.title;
-
-            this.ready = true;
-
-            this.$nextTick(() => document.getElementById('passwordInput').focus());
-        });
+        window.location.reload();
+      });
     }
+  }
 };
 
 </script>
@@ -83,10 +86,6 @@ export default {
     max-width: 480px;
     margin: auto;
     padding: 20px;
-}
-
-.p-fluid > div {
-    margin-bottom: 10px;
 }
 
 </style>
--- frontend/protected.js
+++ frontend/protected.js
@@ -1,13 +1,7 @@
 import { createApp } from 'vue';
 
-import PrimeVue from 'primevue/config';
-import Button from 'primevue/button';
-import InputText from 'primevue/inputtext';
-import Password from 'primevue/password';
-
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
+import tooltip from 'pankow/tooltip';
 
 import './style.css';
 
@@ -15,10 +9,6 @@ import Protected from './Protected.vue';
 
 const app = createApp(Protected);
 
-app.use(PrimeVue);
-
-app.component('Button', Button);
-app.component('Password', Password);
-app.component('InputText', InputText);
+app.directive('tooltip', tooltip);
 
 app.mount('#app');
