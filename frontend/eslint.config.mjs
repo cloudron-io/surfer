5ead0f127a79f72026cd929fc1ddaa37eddaa1c0
Johannes Zellner <johannes@cloudron.io>
2024-11-25 15:52:22 +0100

    Add basic js linting rules for frontend

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,7 +1,9 @@
+import js from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue'
+
 export default [
   // add more generic rulesets here, such as:
-  // js.configs.recommended,
+  js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
   // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
   {
