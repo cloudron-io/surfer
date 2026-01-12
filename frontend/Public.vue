911d2d21c6dd7227e9ab746069f60f1fdc33c59d
Johannes Zellner <johannes@nebulon.de>
2026-01-12 10:56:49 +0100

    Migrate Public.vue to composition style

--- frontend/Public.vue
+++ frontend/Public.vue
@@ -1,3 +1,102 @@
+<script setup>
+
+import { ref, onMounted } from 'vue';
+import { Breadcrumb, Button, Notification, TopBar, fetcher } from '@cloudron/pankow';
+import { sanitize, encode, decode, getPreviewUrl, getExtension } from './utils.js';
+
+import EntryList from './components/EntryList.vue';
+import Preview from './components/Preview.vue';
+
+const ORIGIN = window.location.origin;
+
+const ready = ref(false);
+const path = ref('/');
+const breadcrumbHomeItem = ref({
+  label: '',
+  icon: 'fa-solid fa-house',
+  route: '/'
+});
+const breadcrumbItems = ref([]);
+const entries = ref([]);
+// holds settings values stored on backend
+const settings = ref({
+  folderListingEnabled: false,
+  sortFoldersFirst: false,
+  title: false
+});
+const activeEntry = ref({});
+
+function loadDirectory(folderPath) {
+  activeEntry.value = {};
+
+  folderPath = folderPath ? sanitize(folderPath) : '/';
+
+  // this is injected via ejs in server.js
+  window.surfer.entries.sort(function (a, b) { return a.isDirectory && b.isFile ? -1 : 1; });
+
+  entries.value = window.surfer.entries.map(function (entry) {
+    entry.previewUrl = getPreviewUrl(entry, folderPath);
+    entry.extension = getExtension(entry);
+    entry.rename = false;
+    entry.filePathNew = entry.fileName;
+    return entry;
+  });
+  path.value = folderPath;
+  breadcrumbItems.value = decode(folderPath).split('/').filter(function (e) { return !!e; }).map(function (e, i, a) {
+    return {
+      label: e,
+      route: '#' + sanitize('/' + a.slice(0, i).join('/') + '/' + e)
+    };
+  });
+}
+
+function onEntryOpen(entry) {
+  if (entry.isDirectory) {
+    window.location.pathname = sanitize(path.value + '/' + entry.fileName);
+    return;
+  }
+
+  // TODO open file viewer
+  console.error('To be implemented');
+}
+
+function onSelectionChanged(selectedEntries) {
+  activeEntry.value = selectedEntries[0];
+}
+
+function onPreviewClose() {
+  activeEntry.value = {};
+}
+
+function clearSelection() {
+  activeEntry.value = {};
+}
+
+onMounted(async () => {
+  // global key handler to unset activeEntry
+  window.addEventListener('keyup', (event) => {
+    // only do this if no modal is active - body classlist would be empty
+    if (event.key === 'Escape' && event.target.classList.length === 0) clearSelection();
+  });
+
+  try {
+    const result = await fetcher.get(`${ORIGIN}/api/settings`);
+    settings.value.folderListingEnabled =  !!result.body.folderListingEnabled;
+    settings.value.sortFoldersFirst =  !!result.body.sortFoldersFirst;
+    settings.value.title =  result.body.title;
+  } catch (error) {
+    console.error(error);
+  }
+
+  window.document.title = settings.value.title;
+
+  loadDirectory(decode(window.location.pathname));
+
+  ready.value = true;
+});
+
+</script>
+
 <template>
   <Notification/>
 
@@ -21,123 +120,5 @@
     </div>
   </div>
 </template>
-
-<script>
-
-import { Breadcrumb, Button, Notification, TopBar, fetcher } from '@cloudron/pankow';
-import { sanitize, encode, decode, getPreviewUrl, getExtension } from './utils.js';
-
-import EntryList from './components/EntryList.vue';
-import Preview from './components/Preview.vue';
-
-const ORIGIN = window.location.origin;
-
-export default {
-  name: 'PublicView',
-  components: {
-    Breadcrumb,
-    Button,
-    EntryList,
-    Notification,
-    Preview,
-    TopBar
-  },
-  data() {
-    return {
-      ready: false,
-      busy: false,
-      origin: ORIGIN,
-      domain: window.location.host,
-      path: '/',
-      breadcrumbHomeItem: {
-        label: '',
-        icon: 'fa-solid fa-house',
-        route: '/'
-      },
-      breadcrumbItems: [],
-      entries: [],
-      // holds settings values stored on backend
-      settings: {
-        folderListingEnabled: false,
-        sortFoldersFirst: false,
-        title: false
-      },
-      activeEntry: {}
-    };
-  },
-  async mounted() {
-    // global key handler to unset activeEntry
-    window.addEventListener('keyup', () => {
-      // only do this if no modal is active - body classlist would be empty
-      if (event.key === 'Escape' && event.target.classList.length === 0) this.clearSelection();
-    });
-
-    try {
-      const result = await fetcher.get(`${this.origin}/api/settings`);
-      this.settings.folderListingEnabled =  !!result.body.folderListingEnabled;
-      this.settings.sortFoldersFirst =  !!result.body.sortFoldersFirst;
-      this.settings.title =  result.body.title;
-    } catch (error) {
-      console.error(error);
-    }
-
-    window.document.title = this.settings.title;
-
-    this.loadDirectory(decode(window.location.pathname));
-
-    this.ready = true;
-  },
-    methods: {
-      loadDirectory(folderPath) {
-        this.activeEntry = {};
-
-        folderPath = folderPath ? sanitize(folderPath) : '/';
-
-        // this is injected via ejs in server.js
-        const entries = window.surfer.entries;
-
-        entries.sort(function (a, b) { return a.isDirectory && b.isFile ? -1 : 1; });
-
-        this.entries = entries.map(function (entry) {
-          entry.previewUrl = getPreviewUrl(entry, folderPath);
-          entry.extension = getExtension(entry);
-          entry.rename = false;
-          entry.filePathNew = entry.fileName;
-          return entry;
-        });
-        this.path = folderPath;
-        this.breadcrumbItems = decode(folderPath).split('/').filter(function (e) { return !!e; }).map(function (e, i, a) {
-          return {
-            label: e,
-            route: '#' + sanitize('/' + a.slice(0, i).join('/') + '/' + e)
-          };
-        });
-      },
-      onDownload(entry) {
-        if (entry.isDirectory) return;
-        window.location.href = encode('/api/files/' + sanitize(this.path + '/' + entry.fileName)) + '?access_token=' + localStorage.accessToken;
-      },
-      onEntryOpen: function (entry) {
-        if (entry.isDirectory) {
-          window.location.pathname = sanitize(this.path + '/' + entry.fileName);
-          return;
-        }
-
-        // TODO open file viewer
-        console.error('To be implemented');
-      },
-      onSelectionChanged(selectedEntries) {
-        this.activeEntry = selectedEntries[0];
-      },
-      onPreviewClose() {
-        this.activeEntry = {};
-      },
-      clearSelection() {
-        this.activeEntry = {};
-      }
-    }
-};
-
-</script>
 
 <style></style>
